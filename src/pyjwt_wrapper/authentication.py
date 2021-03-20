322dcd2370d8e7526e0e9408fc3adc019949c348
Nico <nicoc@ovationsgroup.com>
2021-03-20 18:26:34 +0200

    feature: Issue #1 - added creation of refresh token during authentication

--- src/pyjwt_wrapper/authentication.py
+++ src/pyjwt_wrapper/authentication.py
@@ -1,4 +1,7 @@
-from pyjwt_wrapper import Logger, BackEndAuthenticator, generate_jwt, get_utc_timestamp, JWT_SECRET
+from pyjwt_wrapper import Logger, BackEndAuthenticator, generate_jwt, get_utc_timestamp, JWT_SECRET, PASSWORD_SALT
+import json
+import traceback
+import hashlib
 
 
 default_logger = Logger()
@@ -8,8 +11,25 @@ def authentication_service_default_response()->dict:
     return {
         'user_token': None,
         'access_token': None,
+        'refresh_token': None,
         'request_id': ''
     }
+
+
+def create_refresh_token(
+    access_token_data: dict,
+    salt: str=PASSWORD_SALT,
+    refresh_token_ttl: int=86400,
+    request_id: str=None
+)->dict:
+    refresh_token_data = dict()
+    if len(access_token_data) > 0:
+        now = get_utc_timestamp(with_decimal=False)
+        access_token_data_json = json.dumps(access_token_data)
+        access_token_data_json_hash = hashlib.sha256('{}{}'.format(access_token_data_json, PASSWORD_SALT).encode('utf-8')).hexdigest()
+        refresh_token_data['ath'] = access_token_data_json_hash
+        refresh_token_data['exp'] = int(now + refresh_token_ttl)
+    return refresh_token_data
 
 
 def authenticate_using_user_credentials(
@@ -19,9 +39,12 @@ def authenticate_using_user_credentials(
     logger: Logger=default_logger,
     backend: BackEndAuthenticator=BackEndAuthenticator(logger=default_logger),
     request_id: str=None,
-    token_expires_in_seconds: int=86400,
+    token_expires_in_seconds: int=600,
     convert_username_to_lowercase: bool=True,
-    secret_str: str=JWT_SECRET
+    secret_str: str=JWT_SECRET,
+    include_refresh_token: bool=False,
+    refresh_token_salt: str=PASSWORD_SALT,
+    refresh_token_ttl: int=86400
 )->dict:
     if convert_username_to_lowercase:
         username = username.lower()
@@ -51,12 +74,24 @@ def authenticate_using_user_credentials(
             'sub': '{}'.format(username),
             'context': '{}'.format(application_name),
         }
+        refresh_token_data = dict()
+        if include_refresh_token:
+            try:
+                refresh_token_data = create_refresh_token(
+                    access_token_data=access_token_data,
+                    salt=refresh_token_salt,
+                    request_id=request_id
+                )
+            except:
+                logger.error(message='EXCEPTION: {}'.format(traceback.format_exc()), request_id=request_id)
         if len(authentication_backend_call_result.access_token_extra) > 0:
             access_token_data['extra'] = authentication_backend_call_result.access_token_extra
         if len(authentication_backend_call_result.user_token_extra) > 0:
             user_token_data['extra'] = authentication_backend_call_result.user_token_extra
         result['access_token'] = generate_jwt(data=access_token_data, secret_str=secret_str)
         result['user_token'] = generate_jwt(data=user_token_data, secret_str=secret_str)
+        if include_refresh_token and len(refresh_token_data) > 0:
+            result['refresh_token'] = generate_jwt(data=refresh_token_data, secret_str=secret_str)
         logger.info(message='user "{}" authenticated successfully'.format(username), request_id=request_id)
         logger.debug(message='access_token: {}'.format(result['access_token']), request_id=request_id)
         logger.debug(message='user_token: {}'.format(result['user_token']), request_id=request_id)
